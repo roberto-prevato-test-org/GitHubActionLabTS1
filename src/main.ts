69a69330dbff1af89962cdc3c546e0fd40329d81
Roberto Prevato <roberto.prevato@gmail.com>
2020-01-30 20:49:10 +0100

    logging context, creating an instance of GitHub

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import * as core from '@actions/core'
+import {context, GitHub} from '@actions/github'
 import {wait} from './wait'
+
+
+function getPullRequestId(): string {
+  const pullRequestId = process.env.GITHUB_REF;
+
+  if (!pullRequestId)
+    throw new Error("Action was not triggered by a PR. " +
+                    "Use this action with PR triggers.");
+
+  return pullRequestId;
+}
+
 
 async function run(): Promise<void> {
   try {
+    /*
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`)
 
@@ -11,6 +25,18 @@ async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
+    */
+
+    console.info(JSON.stringify(context, null, 4))
+
+    const myToken = core.getInput('myToken');
+
+    const octokit = new GitHub(myToken);
+
+    // TODO: make a request to get all commits associated with the PR (?)
+    const pullRequestId = getPullRequestId();
+    console.log(`PR: ${pullRequestId}`);
+
   } catch (error) {
     core.setFailed(error.message)
   }
