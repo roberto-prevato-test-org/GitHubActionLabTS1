b3dee1f071c92068aa6aec69a2b8df7393809b54
Roberto Prevato <roberto.prevato@gmail.com>
2020-02-02 21:20:16 +0100

    Release/v1 (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,176 @@
 import * as core from '@actions/core'
-import {context, GitHub} from '@actions/github'
-import {wait} from './wait'
+import { context, GitHub } from '@actions/github'
+import {
+  PullsGetResponseLabelsItem,
+  ChecksListSuitesForRefResponse
+} from '@octokit/rest';
+import { WebhookPayload } from '@actions/github/lib/interfaces';
 
 
-function getPullRequestId(): string {
-  const pullRequestId = process.env.GITHUB_REF;
+class NotAPullRequestError extends Error {
+  constructor() {
+    super('Missing pull request data in the context object. ' +
+      'This action must be used with a PR.');
+  }
+}
 
-  if (!pullRequestId)
-    throw new Error("Action was not triggered by a PR. " +
-                    "Use this action with PR triggers.");
 
-  return pullRequestId;
+function requireValue(callback: () => (string | undefined), hint: string): string {
+  const value = callback();
+  if (!value)
+    throw new Error(`Missing value for ${hint}`);
+  return value;
+}
+
+
+function getIssuesIdsFromCommitMessage(message: string): (string[] | null) {
+  if (message.indexOf('#') == -1)
+    return null;
+
+  const match = message.match(/(#\d+)/g);
+
+  if (!match)
+    return null;
+  return match;
+}
+
+
+async function getPullRequestLabels(
+  octokit: GitHub,
+  owner: string,
+  repo: string,
+  pull_number: number
+): Promise<PullsGetResponseLabelsItem[]> {
+  const response = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number
+  });
+
+  return response.data.labels
+}
+
+
+async function runAllChecks(
+  octokit: GitHub,
+  owner: string,
+  repo: string,
+  suites: ChecksListSuitesForRefResponse
+): Promise<void> {
+
+  // console.log(`all_check_suites: ${JSON.stringify(suites, undefined, 2)}`);
+  // console.log('\n\n\n\n\n')
+
+  for (var i = 0; i < suites.check_suites.length; i++) {
+    let checkSuite = suites.check_suites[i];
+    try {
+      await octokit.checks.rerequestSuite({
+        owner,
+        repo,
+        check_suite_id: checkSuite.id
+      })
+    } catch (error) {
+      console.log(`Failed to run check suite ${checkSuite.id}: ${error.message}`);
+    }
+  }
+}
+
+
+function skipValidation(labels: PullsGetResponseLabelsItem[]): boolean {
+  for (var i = 0; i < labels.length; i++) {
+    let label = labels[i];
+    if (label.name == "skip-issue") {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+function isChangeOfLabel(payload: WebhookPayload): boolean {
+  return payload.action == 'labeled' || payload.action == 'unlabeled';
+}
+
+
+async function handleChangeOfLabel(
+  octokit: GitHub,
+  owner: string,
+  repo: string
+): Promise<void> {
+  const ref = requireValue(() => context.payload.pull_request?.head.sha, 'pr_head_sha');
+
+  const suitesForRef = await octokit.checks.listSuitesForRef({
+    owner,
+    repo,
+    ref
+  })
+
+  console.log('Forcing a re-check of previous checks');
+  await runAllChecks(octokit, owner, repo, suitesForRef.data);
+  return;
 }
 
 
 async function run(): Promise<void> {
   try {
-    /*
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const octokit = new GitHub(core.getInput('myToken'));
+    const owner = requireValue(() => context.payload.repository?.owner?.login, 'owner');
+    const repo = requireValue(() => context.payload.repository?.name, 'repository');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const pullRequest = context.payload.pull_request;
 
-    core.setOutput('time', new Date().toTimeString())
-    */
+    if (!pullRequest) {
+      throw new NotAPullRequestError();
+    }
 
-    console.log(JSON.stringify(context, null, 4))
+    if (isChangeOfLabel(context.payload)) {
+      // this action is fired when a PR labels change;
+      // since GitHub creates a new check, pass this one and force a re-check of
+      // previously failed checks
+      await handleChangeOfLabel(
+        octokit,
+        owner,
+        repo,
+      );
+      return;
+    }
 
-    const myToken = core.getInput('myToken');
+    const labels = await getPullRequestLabels(octokit, owner, repo, pullRequest.number);
 
-    const octokit = new GitHub(myToken);
+    if (skipValidation(labels)) {
+      console.log("Commit messages validation skipped by label (skip-issue)");
+      return;
+    }
 
-    // TODO: make a request to get all commits associated with the PR (?)
-    const pullRequestId = getPullRequestId();
-    console.log(`PR: ${pullRequestId}`);
+    // NB: paginate fetches all commits for the PR, so it handles
+    // the unlikely situation of a PR with more than 250 commits
+    await octokit
+      .paginate('GET /repos/:owner/:repo/pulls/:pull_number/commits',
+        {
+          owner,
+          repo,
+          pull_number: pullRequest.number
+        }
+      )
+      .then(items => {
+        var anyMissing = false;
 
-    const payload = JSON.stringify(context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+        items.forEach(item => {
+          const issuesIds = getIssuesIdsFromCommitMessage(item.commit.message);
 
+          if (!issuesIds) {
+            anyMissing = true;
+            console.error(`Commit ${item.sha} with message "${item.commit.message}"
+                           does not refer any issue.`)
+          } else {
+            console.info(`ids: ${issuesIds}`)
+          }
+        });
+
+        if (anyMissing) {
+          throw new Error("One or more commit messages don't refer any issue.");
+        }
+      })
   } catch (error) {
     core.setFailed(error.message)
   }
